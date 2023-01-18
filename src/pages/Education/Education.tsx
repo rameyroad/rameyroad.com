43c5c7cb1c73601b8ccdffc5ff093868157e41da
Jason Moore <jason@jmit.us>
2023-01-18 10:43:47 -0500

    Image fix

--- src/pages/Education/Education.tsx
+++ src/pages/Education/Education.tsx
@@ -26,64 +26,39 @@ export const Education: React.FC = () => {
                                 height="146"
                             />
                         </p>
-                        <h4>
-                            Master of Science - Information Systems &amp;
-                            Operations Management
-                        </h4>
+                        <h4>Master of Science - Information Systems &amp; Operations Management</h4>
                         <p>
                             Received MS degree from the
-                            <a
-                                href="https://bryan.uncg.edu/"
-                                target="_blank"
-                                rel="noopener"
-                            >
+                            <a href="https://bryan.uncg.edu/" target="_blank" rel="noopener">
                                 Bryan School of Business
                             </a>
-                            at the University of North Carolina at Greensboro in
-                            August of 2005.
+                            at the University of North Carolina at Greensboro in August of 2005.
                         </p>
                         <ul>
                             <li>Graduated summa cum laude (3.92/4.00)</li>
-                            <li>
-                                Academic Excellence award for outstanding
-                                academic achievement
-                            </li>
+                            <li>Academic Excellence award for outstanding academic achievement</li>
                         </ul>
                     </div>
                     <div className="col-12">
                         <h4>
-                            <img
-                                style={{ padding: 15, float: "right" }}
-                                src="/uploads/81b2b5e9-03a3-487a-ad1e-5a69969ae31f-East_Tennessee_State_Buccaneers_logo.png"
-                                alt=""
-                                width=""
-                                height="200"
-                            />
-                        </h4>
-                        <h4>Bachelor of Science - Computer Science</h4>
-                        <p>
-                            Received BS degree from
-                            <a
-                                href="https://www.etsu.edu/cbat/computing/"
-                                target="_blank"
-                                rel="noopener"
-                            >
-                                East Tennessee State University
-                            </a>
-                            in August of 1998.
                             <img
                                 style={{ float: "right" }}
                                 src={`${contentRoot}/images/East_Tennessee_State_Buccaneers_logo.png`}
                                 width="150"
                                 height="149"
                             />
+                        </h4>
+                        <h4>Bachelor of Science - Computer Science</h4>
+                        <p>
+                            Received BS degree from
+                            <a href="https://www.etsu.edu/cbat/computing/" target="_blank" rel="noopener">
+                                East Tennessee State University
+                            </a>
+                            in August of 1998.
                         </p>
                         <ul>
                             <li>Graduated cum laude (3.53/4.00)</li>
-                            <li>
-                                Member of Upsilon Pi Epsilon Computer Science
-                                Honor Society
-                            </li>
+                            <li>Member of Upsilon Pi Epsilon Computer Science Honor Society</li>
                         </ul>
                     </div>
                 </div>
