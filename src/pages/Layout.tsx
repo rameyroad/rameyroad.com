a4c6117e3f451152ab5c43dd7283081f7faa1aa5
Jason Moore <jason@jmit.us>
2023-02-10 11:34:30 -0500

    Added Google Analytics

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+import ReactGA from "react-ga";
 
 import { Footer } from "./shared/Footer";
 import { Header } from "./shared/Header";
 import { BackToTop } from "./shared/BackToTop";
 
 export const Layout: React.FC<any> = (props: any) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        ReactGA.initialize("353233475");
+    }, []);
+
+    useEffect(() => {
+        ReactGA.pageview(location.pathname + location.search);
+    }, [location]);
+
     return (
         <Fragment>
             <div id="mainPage">
